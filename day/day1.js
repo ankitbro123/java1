96117771da18f4d1da3348c0f21b399a5f1edfda
ankitbro123 <ankitbra123@gmail.com>
2024-11-05 03:02:19 +0000

    this is day to of (comparision)

--- day/day1.js
+++ day/day1.js
@@ -1,2 +1,6 @@
 console.log("hello javascript");
 console.log("kya he re");
+
+let value=3;
+ let negvalue = --value;
+ console.log(negvalue);
---
+++ day/day2.js
@@ -0,0 +1,20 @@
+// let ankit="soby"
+// console.log(typeof ankit);
+
+// let num="33"
+// let numa=String(num);
+// console.log(typeof numa)
+// console.log(numa);
+
+// console.log("2"+2+6);
+// console.log(5+2+89+"6");
+// console.log(5+2+"5"+"6");
+// console.log("5"+2+5+"6");
+
+// console.log(5+5 +"90"+5 +8)
+// for(cr of "javascript"){
+//     console.log(cr);
+// }
+// console.log(null>=0);
+// console.log(null==null);
+// console.log(null==0);
